f08d24c79cc26b7b3a7add9c69b574381989739a
Matheus Felber <math.felber@gmail.com>
2024-09-28 21:31:52 -0300

    backupp

--- server.js
+++ server.js
@@ -1,30 +1,39 @@
-/* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
-/* ***********************
- * Require Statements
- *************************/
 const express = require("express")
-const env = require("dotenv").config()
+const expressLayouts = require("express-ejs-layouts")
+const path = require("path")
+
 const app = express()
-const static = require("./routes/static")
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
+app.use(expressLayouts)
+app.set("layout", "layouts/layout")
+
+/* ***********************
+ * Static Files Middleware
+ *************************/
+app.use(express.static(path.join(__dirname, "public")))
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+// Render the home (index) view
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Home' })
+})
 
 /* ***********************
  * Local Server Information
- * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 5500
+const host = process.env.HOST || 'localhost'
 
 /* ***********************
- * Log statement to confirm server operation
+ * Start the Server
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`Server running at http://${host}:${port}`)
 })
